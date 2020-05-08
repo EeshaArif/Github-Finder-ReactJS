8fbfdcefb8aa39c0d94a57fcc455bef8e58dc391
EeshaArif <eeshaarif@gmail.com>
2020-05-08 14:56:56 +0500

    bootstrap-classes

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,14 +3,12 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "http://picsum.photos/200", // Generatez a random picture
   };
   render() {
     return (
       <React.Fragment>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
