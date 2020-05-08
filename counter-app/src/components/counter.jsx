08da41ec9f6d8850a446ebb3defa69ab145a46c6
EeshaArif <eeshaarif@gmail.com>
2020-05-08 14:47:58 +0500

    ImageUrl-state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,16 +3,12 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-
-    /*
-      address: {
-          street: ''
-      }
-      */
+    imageUrl: "http://picsum.photos/200", // Generatez a random picture
   };
   render() {
     return (
       <React.Fragment>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
@@ -21,7 +17,7 @@ class Counter extends Component {
 
   formatCount() {
     const { count } = this.state;
-    return count == 0 ? "zero" : count;
+    return count == 0 ? "Zero" : count;
   }
 }
 
