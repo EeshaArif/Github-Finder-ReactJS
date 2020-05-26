a3e9b634637686fdb4ec289161d6cf6c1ad582c1
EeshaArif <eeshaarif@gmail.com>
2020-05-26 21:33:14 +0500

    bind event handler to this

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,27 +3,37 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: [],
+    tags: ["tag1", "tag2", "tag3"],
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  constructor() {
+    super();
+    // this will reference the current object
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+  handleIncrement() {
+    console.log("Increment Count", this.state.count);
+    // obj.method(); -> returns reference to an object
+    // function(); -> returns reference to the window object
   }
   render() {
-    return <React.Fragment>{this.renderTags()}</React.Fragment>;
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
   }
 
-  /*
-
-<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-<button className="btn btn-secondary btn-sm">Increment</button>
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
@@ -34,7 +44,6 @@ class Counter extends Component {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
-
-*/
 }
+
 export default Counter;
