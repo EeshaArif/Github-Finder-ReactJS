86f3a34b371464f5383a9ddd644f660de673a233
EeshaArif <eeshaarif@gmail.com>
2020-05-27 01:58:44 +0500

    moved the state upwards to the app

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,18 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 // import logo from './logo.svg';
 import "./App.css";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar></NavBar>
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
-}
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 3 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleDelete = (counterID) => {
+    console.log("Event Handler called", counterID);
+    const counters = this.state.counters.filter((c) => c.id !== counterID);
+    this.setState({ counters: counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
 
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        ></NavBar>
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            onReset={this.handleReset}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
 export default App;
