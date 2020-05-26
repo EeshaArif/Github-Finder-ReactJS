b9f1abbc4bb37a101be765ddaae7ff26b5493929
EeshaArif <eeshaarif@gmail.com>
2020-05-26 23:13:05 +0500

    Refactoring to keep changes at one component

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,27 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-  };
-
-  handleIncrement = (product) => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 }); // you have to tell react what has changed
-  };
-
   render() {
     // Every react component has a property called props
     return (
       <div>
-        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           // just use arrow functions if you want to pass arguments
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -29,12 +25,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
