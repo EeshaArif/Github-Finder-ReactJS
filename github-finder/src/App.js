f6d911b0298f0df124baac1a6d65a7dabbe475cd
EeshaArif <eeshaarif@gmail.com>
2020-05-27 22:43:20 +0500

    UserItems.jxs-Users.jsx

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./layouts/Navbar";
-import UserItems from "./components/users/UserItems";
+import Users from "./components/users/Users";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar></Navbar>
-        <UserItems />
+        <Users />
       </div>
     );
   }
--- github-finder/src/components/users/UserItems.jsx
+++ github-finder/src/components/users/UserItems.jsx
@@ -1,17 +1,8 @@
 import React, { Component } from "react";
 
 class UserItems extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: "id",
-      login: "octocat",
-      avatar_url: "https://avatars1.githubusercontent.com/u/583231?s=400&v=4",
-      html_url: "https://github.com/octocat",
-    };
-  }
   render() {
-    const { login, avatar_url, html_url } = this.state;
+    const { login, avatar_url, html_url } = this.props.user;
     return (
       <div className="card text-center">
         <img
