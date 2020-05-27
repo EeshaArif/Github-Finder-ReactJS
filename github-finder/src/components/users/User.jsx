b7d2822de6fe32acbc5e987ed6e03ad6946e5be0
EeshaArif <eeshaarif@gmail.com>
2020-05-28 04:10:48 +0500

    back to search

--- github-finder/src/components/users/User.jsx
+++ github-finder/src/components/users/User.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
-
+import Spinner from "../../layouts/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
   }
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
   render() {
     const {
       name,
@@ -20,7 +27,14 @@ export class User extends Component {
       hireable,
     } = this.props.user;
     const { loading } = this.props;
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+    return (
+      <React.Fragment>
+        <Link to="/" className="btn btn-light">
+          Back To Search
+        </Link>
+      </React.Fragment>
+    );
   }
 }
 
