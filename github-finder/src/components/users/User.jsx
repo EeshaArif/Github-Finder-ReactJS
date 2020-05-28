f87755f1fb8eef0c288fb3bc7d57fdee96dc9895
EeshaArif <eeshaarif@gmail.com>
2020-05-28 10:05:01 +0500

    refactored user.jsx

--- github-finder/src/components/users/User.jsx
+++ github-finder/src/components/users/User.jsx
@@ -1,105 +1,103 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import Spinner from "../../layouts/Spinner";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Repos from "../repos/Repos";
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login);
-    this.props.getUserRepos(this.props.match.params.login);
-  }
-  static propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
-  };
-  render() {
-    const {
-      name,
-      avatar_url,
-      location,
-      bio,
-      blog,
-      login,
-      html_url,
-      company,
-      followers,
-      following,
-      public_repos,
-      public_gists,
-      hireable,
-    } = this.props.user;
-    const { loading, repos } = this.props;
-    if (loading) return <Spinner />;
-    return (
-      <React.Fragment>
-        <Link to="/" className="btn btn-light">
-          Back To Search
-        </Link>
-        Hireable:{""}
-        {hireable ? (
-          <i className="fas fa-check text-success" />
-        ) : (
-          <i className="fas fa-times-circle text-danger" />
-        )}
-        <div className="card grid-2">
-          <div className="all-center">
-            <img
-              src={avatar_url}
-              className="round-img"
-              alt=""
-              style={{ width: "150px" }}
-            />
-            <h1>{name}</h1>
-            <p>Location: {location}</p>
-          </div>
-          <div>
-            {bio && (
-              <React.Fragment>
-                <h3>Bio:</h3>
-                <p>{bio}</p>
-              </React.Fragment>
-            )}
-            <a href={html_url} className="btn btn-dark my-1">
-              Visit Github Profile
-            </a>
-            <ul>
-              <li>
-                {login && (
-                  <React.Fragment>
-                    <strong>Username: </strong> {login}
-                  </React.Fragment>
-                )}
-              </li>
-              <li>
-                {company && (
-                  <React.Fragment>
-                    <strong>Company: </strong> {company}
-                  </React.Fragment>
-                )}
-              </li>
-              <li>
-                {blog && (
-                  <React.Fragment>
-                    <strong>Website: </strong> {blog}
-                  </React.Fragment>
-                )}
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="card card-center">
-          <div className="badge badge-primary">Followers: {followers}</div>
-          <div className="badge badge-success">Following: {following}</div>
-          <div className="badge badge-light">Public Repos: {public_repos}</div>
-          <div className="badge badge-dark">Public Gists: {public_gists}</div>
-        </div>
-        <Repos repos={repos}></Repos>
-      </React.Fragment>
-    );
-  }
-}
+const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+  }, []);
 
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    company,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+  } = user;
+
+  if (loading) return <Spinner />;
+  return (
+    <React.Fragment>
+      <Link to="/" className="btn btn-light">
+        Back To Search
+      </Link>
+      Hireable:{""}
+      {hireable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            className="round-img"
+            alt=""
+            style={{ width: "150px" }}
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
+        </div>
+        <div>
+          {bio && (
+            <React.Fragment>
+              <h3>Bio:</h3>
+              <p>{bio}</p>
+            </React.Fragment>
+          )}
+          <a href={html_url} className="btn btn-dark my-1">
+            Visit Github Profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <React.Fragment>
+                  <strong>Username: </strong> {login}
+                </React.Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <React.Fragment>
+                  <strong>Company: </strong> {company}
+                </React.Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <React.Fragment>
+                  <strong>Website: </strong> {blog}
+                </React.Fragment>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="card card-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+      <Repos repos={repos}></Repos>
+    </React.Fragment>
+  );
+};
+User.propTypes = {
+  loading: PropTypes.bool,
+  user: PropTypes.object.isRequired,
+  repos: PropTypes.array.isRequired,
+  getUser: PropTypes.func.isRequired,
+  getUserRepos: PropTypes.func.isRequired,
+};
 export default User;
