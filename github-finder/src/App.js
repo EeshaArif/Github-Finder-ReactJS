613b75f95a312a8beac14bb12cfa581cf0bb9e54
EeshaArif <eeshaarif@gmail.com>
2020-05-28 23:12:17 +0500

    Home.jsx-NotFound.jsx

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./layouts/Navbar";
-import Users from "./components/users/Users";
+import Home from "./components/pages/Home";
 import User from "./components/users/User";
 import Alert from "./layouts/Alert";
 import About from "./components/pages/About";
-import Search from "./components/users/Search";
+import NotFound from "./components/pages/NotFound";
 import GithubState from "./contexts/github/githubState";
 import AlertState from "./contexts/alert/AlertState";
 
@@ -20,18 +20,10 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
